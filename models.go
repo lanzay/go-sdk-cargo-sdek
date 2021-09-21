0f09fe5423be2f2205026f97077d648a6df97886
Lnazay <lanzay@gmail.com>
2021-09-21 14:10:39 +0300

    Location

--- models.go
+++ models.go
@@ -201,3 +201,45 @@ type (
 		City     string `json:"city"`
 	}
 )
+
+type PVZ struct {
+	Code                string              `json:"code"`
+	Name                string              `json:"name"`
+	Location            Location            `json:"location"`
+	AddressComment      string              `json:"address_comment"`
+	NearestStation      string              `json:"nearest_station"`
+	NearestMetroStation string              `json:"nearest_metro_station"`
+	WorkTime            string              `json:"work_time"`
+	Phones              []Phone             `json:"phones"`
+	Email               string              `json:"email"`
+	Note                string              `json:"note"`
+	Type                string              `json:"type"`
+	OwnerCode           string              `json:"owner_code"`
+	TakeOnly            bool                `json:"take_only"`
+	IsDressingRoom      bool                `json:"is_dressing_room"`
+	HaveCashless        bool                `json:"have_cashless"`
+	HaveCash            bool                `json:"have_cash"`
+	AllowedCod          bool                `json:"allowed_cod"`
+	WorkTimeList        []WorkTimeList      `json:"work_time_list"`
+	WorkTimeExceptions  []WorkTimeException `json:"work_time_exceptions"`
+	WeightMin           float32             `json:"weight_min,omitempty"`
+	WeightMax           float32             `json:"weight_max,omitempty"`
+	Errors              []ErrorSDK          `json:"errors"`
+}
+
+type Location struct {
+	Code        string  `json:"code"`
+	FiasGUID    string  `json:"fias_guid"`
+	PostalCode  string  `json:"postal_code"`
+	Longitude   float32 `json:"longitude"`
+	Latitude    float32 `json:"latitude"`
+	CountryCode string  `json:"country_code"`
+	Region      string  `json:"region"`
+	RegionCode  int     `json:"region_code"`
+	SubRegion   string  `json:"sub_region"`
+	CityCode    int     `json:"city_code"`
+	City        string  `json:"city"`
+	KladrCode   string  `json:"kladr_code"`
+	Address     string  `json:"address"`
+	AddressFull string  `json:"address_full"`
+}
--- pvz-service.go
+++ pvz-service.go
@@ -5,44 +5,6 @@ import (
 	"reflect"
 )
 
-type PVZ struct {
-	Code                string              `json:"code"`
-	Name                string              `json:"name"`
-	Location            Location            `json:"location"`
-	AddressComment      string              `json:"address_comment"`
-	NearestStation      string              `json:"nearest_station"`
-	NearestMetroStation string              `json:"nearest_metro_station"`
-	WorkTime            string              `json:"work_time"`
-	Phones              []Phone             `json:"phones"`
-	Email               string              `json:"email"`
-	Note                string              `json:"note"`
-	Type                string              `json:"type"`
-	OwnerCode           string              `json:"owner_code"`
-	TakeOnly            bool                `json:"take_only"`
-	IsDressingRoom      bool                `json:"is_dressing_room"`
-	HaveCashless        bool                `json:"have_cashless"`
-	HaveCash            bool                `json:"have_cash"`
-	AllowedCod          bool                `json:"allowed_cod"`
-	WorkTimeList        []WorkTimeList      `json:"work_time_list"`
-	WorkTimeExceptions  []WorkTimeException `json:"work_time_exceptions"`
-	WeightMin           float32             `json:"weight_min,omitempty"`
-	WeightMax           float32             `json:"weight_max,omitempty"`
-	Errors              []ErrorSDK          `json:"errors"`
-}
-
-type Location struct {
-	CountryCode string  `json:"country_code"`
-	RegionCode  int     `json:"region_code"`
-	Region      string  `json:"region"`
-	CityCode    int     `json:"city_code"`
-	City        string  `json:"city"`
-	PostalCode  string  `json:"postal_code"`
-	Longitude   float32 `json:"longitude"`
-	Latitude    float32 `json:"latitude"`
-	Address     string  `json:"address"`
-	AddressFull string  `json:"address_full"`
-}
-
 type Phone struct {
 	Number string `json:"number"`
 }
